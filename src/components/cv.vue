cf55b3e6dfa766397f00bb593e9e6f8b957bd13c
Ricardo van der Heijden <ricardovdheijden@hotmail.com>
2017-07-22 18:47:34 +0200

    Setup cv route and added data json file

---
+++ src/components/cv.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="cv">
+    Hello CV
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cv',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import cv from '@/components/cv'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'cv',
+      component: cv
     }
   ]
 })
