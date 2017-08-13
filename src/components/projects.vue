20af6a7f399ee897bfb95ff27c29c4870e2b15f6
Ricardo van der Heijden <ricardovdheijden@hotmail.com>
2017-08-13 13:47:59 +0200

    MP-003 Removed vue-strap modal

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -37,25 +37,6 @@
         </div>
       </div>
     </div>
-    <button data-toggle="modal" data-target="#myModal" class="btn btn-primary">
-      My Button
-    </button>
-    <div class="modal fade" id="myModal" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">My Modal</h4>
-          </div>
-          <div class="modal-body">
-            <p>My Modal Body</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
   </div>
 </template>
