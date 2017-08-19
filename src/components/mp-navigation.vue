32377bd6a911696bf6901e3595163f7ab3c37cad
Ricardo van der Heijden <ricardovdheijden@hotmail.com>
2017-08-19 22:48:38 +0200

    MP-005 Added navbar

--- src/components/mp-navigation.vue
+++ src/components/mp-navigation.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    Navigation for my-portfolio
+  <div class="navbar navbar-default ">
+    <div class="container">
+      <ul class="nav nav-pills">
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#cv">CV</a></li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -9,5 +14,10 @@
 </script>
 
 <style scoped>
-
+.navbar {
+  background-color: white;
+  margin-bottom: 0;
+  border-radius: 0;
+  border: 0;
+}
 </style>
