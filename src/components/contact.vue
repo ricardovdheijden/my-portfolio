8ab7c2c6bf1ae8047f343c85fc890ce00b379d4e
Ricardo van der Heijden <ricardovdheijden@hotmail.com>
2017-09-29 00:09:51 +0200

    MP-008 Changed style and photo of contact page

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,58 +1,76 @@
 <template>
-  <div class="container">
-    <div class="contact row row-eq-height">
-      <div class="col-sm-6 image">
-        <img class="logo" src="../assets/pen.png">
+  <div class="contact">
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Contact</h1>
       </div>
-      <div class="col-sm-6 contacts">
-        <div class="row">
-          <div class="col-xs-12">
-            <h1>{{contactData.title}}</h1>
-            <p>{{contactData.introduction}}</p>
-          </div>
-        </div>
-        <div class="row contact-item" v-for="contact in contactData.contacts">
-          <div class="col-xs-2">
-            <img class="logo" :src="contact.logo">
-          </div>
+    </div>
+
+    <div class="container contact-section">
+      <h2>{{contactData.introduction}}</h2>
+      <div class="row container contact-item" v-for="contact in contactData.contacts">
+        <div class="">
+          <h3><img class="logo" :src="contact.logo"><a v-bind:href="contact.link" target="_blank">{{ contact.text }}</a></h3>
         </div>
       </div>
     </div>
+
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-export default {
-  name: 'contact',
-  data () {
-    return {
-      contactData: {}
-    }
-  },
-  mounted () {
-    this.$store.state.activePage = 'contact'
-    let self = this
-    axios.get('static/json/contact-data.json')
-      .then(function (response) {
-        self.contactData = response.data
-      })
+  import axios from 'axios'
+  export default {
+    name: 'contact',
+    data () {
+      return {
+        contactData: {}
+      }
+    },
+    mounted () {
+      this.$store.state.activePage = 'contact'
+      let self = this
+      axios.get('static/json/contact-data.json')
+        .then(function (response) {
+          self.contactData = response.data
+        })
+    },
+    methods: {}
   }
-}
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .jumbotron {
+    background-image: url('../assets/iPhone.jpg');
+    background-repeat: no-repeat;
+    background-position: left;
+    min-height: 350px;
+    background-size: cover;
+  }
+
+  .jumbotron > .container {
+    color: white;
+  }
+
   img {
-    width: 100%;
+    width: 50px;
+    height: 50px;
+    margin-right: 20px;
   }
-  .image {
-    padding-left: 0;
-    padding-right: 0;
+
+  /* Bootstrap grid adjustments */
+  /* col-sm and up */
+  @media (min-width: 768px) {
+    .jumbotron {
+      background-position: bottom;
+    }
   }
-  .contact {
-    margin-bottom: 40px;
-  }
-  .contact-item {
-    margin-bottom: 20px;
+
+  /* col-lg and up */
+  @media (min-width: 1200px) {
+    .jumbotron {
+      background-position: 0 -40px;
+    }
   }
 </style>
